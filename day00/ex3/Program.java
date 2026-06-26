b767e01ce23bd1076f272235dc923a7d0df90954
aminekell <aminekella2003@gmail.com>
2026-06-26 23:03:26 +0100

    finish ex03 day 00 java

--- day00/ex3/Program.java
+++ day00/ex3/Program.java
@@ -5,66 +5,93 @@ class Program {
       
         Scanner scanner = new Scanner(System.in);
 
-        int Weak_count = 0;
+        int week_count = 1;
         long hold = 0;
+        System.out.print("-> ");
 
         
         while(scanner.hasNext())
             { 
-                 System.out.print("-> ");
-                String weak = scanner.next();
-
-                if(weak.equals("42") && Weak_count < 18)
-                {
-                        hold /= 10;
+                String week = scanner.next();
+                
+                if(week.equals("42"))
+                    {
+                        if(week_count < 18)
+                            hold /= 10;
                         break;
-                }
-
-                if(!weak.equals("weak"))
-                {
-                    System.err.println("IllegalArgument");
-                    System.exit(-1);
-                }
-                if(!scanner.hasNextInt())
-                {
-                    System.err.println("IllegalArgument");
-                    System.exit(-1);
-                }
-                int current_weak = scanner.nextInt();
-                if(current_weak != Weak_count)
-                {
-                    System.err.println("IllegalArgument");
-                    System.exit(-1);
-                }
-                Weak_count++;
-
-                if(Weak_count > 18)
+                    }
+                    
+                    if(!week.equals("Week"))
                         {
                             System.err.println("IllegalArgument");
                             System.exit(-1);
                         }
-
-                int value = 9;
-
-                for(int i = 0; i < 5 ; i++)
-                {
-                    int save = scanner.nextInt();
-                    if(save < value)
-                        value = save;
-                }
-                hold = (hold + value);
-                if(Weak_count < 18)
-                    hold *= 10;
-            }
+                        if(!scanner.hasNextInt())
+                            {
+                                System.err.println("IllegalArgument");
+                                System.exit(-1);
+                            }
+                int current_week = scanner.nextInt();
+                            if(current_week != week_count)
+                                {
+                                    System.err.println("IllegalArgument");
+                                    System.exit(-1);
+                                }
+                                
+                                if(week_count > 18)
+                                    {
+                                        System.err.println("IllegalArgument");
+                                        System.exit(-1);
+                                    }
+                                    
+                                    int value = 9;
+                                    System.out.print("-> ");
+                                    
+                                    for(int i = 0; i < 5 ; i++)
+                                    {
+                                            if (!scanner.hasNextInt()) {
+                                            System.err.println("IllegalArgument");
+                                            System.exit(-1);
+                                        }
+                                        int save = scanner.nextInt();
+                                        if (save == 0)
+                                                {
+                                                    System.err.println("IllegalArgument");
+                                                    System.exit(-1);
+                                                }
+                                            if(save < value)
+                                                value = save;
+                                        }
+                                        hold = (hold + value);
+                                        if(week_count < 18)
+                                            hold *= 10;
+                                    week_count++;
+                                    System.out.print("-> ");
+                    }
             long new_s = 0;
 
             while(hold != 0)
-            {
-                new_s = hold % 10;
-                hold /= 10;
-            }
-            System.out.println(hold);
-            System.out.println(new_s);
+                {
+                    new_s = (new_s * 10) + (hold % 10);
+                    hold /= 10;
+                }
+            
+                int weeks = 1;
+                while (new_s != 0)
+                {
+                    System.out.print("   Week " + weeks + " ");
+
+                    for(int i = 0; i < (new_s % 10); i++)
+                    {
+                        System.out.print("=");
+                    }
+                    System.out.println(">");
+                    new_s /= 10;
+                    weeks++;
+                }
+
+
+            scanner.close();
             
    }
 }
