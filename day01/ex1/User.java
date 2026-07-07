f3316a3ea336443eaf89fa835b13cfec63b07107
aminekell <aminekella2003@gmail.com>
2026-07-07 16:22:58 +0100

    test

--- day01/ex1/User.java
+++ day01/ex1/User.java
@@ -1,1 +1,54 @@
-package day01.ex1;
+public class User {
+    private Integer id;
+    private String Name;
+    private double Balance;
+
+    public User(String _Name, double _Balance) {
+        if (Balance < 0) {
+            System.err.print("Balnace most be possitive !!");
+            System.exit(-1);
+        }
+        id = UserIdsGenerator.getInstance().generateId();
+        SetName(_Name);
+        SetBalance(_Balance);
+    }
+
+    // getter and setter for the id
+    public Integer Getid() {
+        return id;
+    }
+
+    public void Setid(Integer Id) {
+        id = Id;
+    }
+
+    // getter and setter for the name
+
+    public String GetName() {
+        return Name;
+    }
+
+    public void SetName(String _Name) {
+        Name = _Name;
+    }
+
+    // getter and setter for the balance
+
+    public double GetBalance() {
+        return Balance;
+    }
+
+    public void SetBalance(double _Balance) {
+        if (_Balance < 0) {
+            System.err.print("Balnace most be possitive !!");
+            System.exit(-1);
+        }
+        Balance = _Balance;
+    }
+
+}
+
+// The difference between int and Integer in Java is that int is a primitive
+// data type. It stores the value directly in memory and does not provide any
+// methods. Integer is an object (a wrapper class) that stores an int value and
+// provides useful methods for working with integers.
